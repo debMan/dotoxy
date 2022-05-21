581c7ca672a578555fe319589553446916d7a5e6
MohamadAli <mrht74@yahoo.com>
2022-05-21 19:55:49 +0430

    Added samples and proxy-async.py

---
+++ proxy-async.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import asyncio, socket, ssl
+
+
+BIND_ADDRESS = ""
+BIND_PORT = 12345  # Port to listen on (non-privileged ports are > 1023)
+UPSTREAM_HOST='1.1.1.1'
+UPSTREAM_PORT=853
+UPSTREAM_CN = 'cloudflare-dns.com'
+# UPSTREAM_CAFILE=None
+
+# TODO: Fix this to run in async
+async def upstream(query, host, port, cn, cafile=None):
+    purpose = ssl.Purpose.SERVER_AUTH
+    context = ssl.create_default_context(purpose, cafile=cafile)
+    raw_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    raw_sock.setblocking(False)
+    raw_sock.connect((host, port))
+    # print('Connected to host {} and port {} to query DNS'.format(host, port))
+    ssl_sock = context.wrap_socket(raw_sock, server_hostname=cn)
+    ssl_sock.sendall(query)
+    response = ssl_sock.recv(1024)
+    return response
+
+async def handle_client(reader, writer):
+    while True:
+        query = await reader.read(1024)
+        if not query:
+            break
+        response = await upstream(
+            query, 
+            UPSTREAM_HOST, 
+            UPSTREAM_PORT, 
+            UPSTREAM_CN
+        )
+        writer.write(response)
+        await writer.drain()
+    writer.close()
+
+async def run_server():
+    server = await asyncio.start_server(handle_client, BIND_ADDRESS, BIND_PORT)
+    async with server:
+        await server.serve_forever()
+
+if __name__ == "__main__":
+    asyncio.run(run_server())
